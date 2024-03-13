ae7781f750a36a6edf58b987ce6615e3b64d544c
Atahan <atahan11951195@gmail.com>
2024-03-13 17:48:55 +0400

    add cart view 01:05

--- carts/urls.py
+++ carts/urls.py
@@ -4,8 +4,8 @@ from .views import cart_add, cart_remove, cart_change
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', cart_add, name='add'),
-    path('cart_remove/<int:product_id>/', cart_remove, name='remove'),
-    path('cart_change/<int:product_id>/', cart_change, name='change'),
+    path('cart_add/<slug:product_slug>/', cart_add, name='add'),
+    path('cart_remove/<slug:product_slug>/', cart_remove, name='remove'),
+    path('cart_change/<slug:product_slug>/', cart_change, name='change'),
 ]
 

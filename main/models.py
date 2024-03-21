ac97809e4e4c1ee2ca962eff9500c9b8174269fc
Atahan <atahan11951195@gmail.com>
2024-03-21 09:02:22 +0400

    asdasdasdadas

--- main/models.py
+++ main/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class About(models.Model):
 
-    phone = models.CharField(max_length=15)
-    about = models.TextField()
-    address = models.URLField(max_length=200)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    about = models.TextField(max_length=200, blank=True, null=True)
+    address = models.URLField(max_length=200, blank=True, null=True)
+    telegram = models.URLField(max_length=200, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'About'

41f37ff829494df99ba987872fd416dc9526e335
Atahan <atahan11951195@gmail.com>
2024-03-21 05:08:16 +0400

    query

--- goods/utils.py
+++ goods/utils.py
@@ -3,23 +3,22 @@ from django.db.models import Q
 
 
 def q_search(query):
-
     """
     This function is used to search for products in the database.
-     It takes a query and returns a list of products that match the query.
-      The query can be a string or an integer.
-       If the query is an integer, it is assumed to be a product id and the function returns
-        a list of products with that id. If the query is a string, the function searches for products with
-         a name or description that contains the query string. The function uses the Q object to create a query
-          that matches products with a name or description that contains any of the words in the query string.
-           The function returns a list of products that match the query. If no products match the query,
-            the function returns an empty list. The function is used in the search view to search for products
-             in the database. The function is also used in the product detail view to search for related products.
-
+    It takes a query and returns a list of products that match the query.
+    The query can be a string or an integer.
+    If the query is an integer, it is assumed to be a product id and the function returns
+    a list of products with that id. If the query is a string, the function searches for products with
+    a name or description that contains the query string. The function uses the Q object to create a query
+    that matches products with a name or description that contains any of the words in the query string.
+    The function returns a list of products that match the query. If no products match the query,
+    the function returns an empty list. The function is used in the search view to search for products
+    in the database. The function is also used in the product detail view to search for related products.
     """
 
-    if query.isdigit and len(query) <= 4:
+    if query.isdigit() and len(query) <= 4:
         return Product.objects.filter(id=query)
-    return Product.objects.filter(Q(description__search=query)) | Product.objects.filter(Q(name__search=query))
+    else:
+        return Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
 
--- goods/views.py
+++ goods/views.py
@@ -4,31 +4,69 @@ from .models import Category, Product
 from .utils import q_search
 
 
-def catalog(request, category_slug=None):
+# def catalog(request, category_slug=None):
+#
+#     """
+#     Catalog view function that displays a list of products in a category.
+#         The function takes a request and a category slug as input.
+#          The function gets the category with the given slug from the database.
+#         If the category slug is 'all', the function gets all products from the database.
+#          If the category slug is not 'all', the function gets all products in the category from the database.
+#           The function gets the page number from the request. The function gets the on_sale parameter from the request.
+#            The function gets the order_by parameter from the request.
+#             The function gets the query parameter from the request.
+#              If the on_sale parameter is True, the function filters the products to only include products that
+#               are on sale.
+#               If the order_by parameter is not 'default', the function orders the products by the given parameter.
+#                If the query parameter is not None, the function searches for products that match the query.
+#                 The function uses the q_search function to search for products that match the query.
+#                  The function paginates the products and returns a list of products for the current page.
+#                   The function returns a render of the catalog.html template with the list of products
+#                    for the current page.
+#                     The function is used in the catalog view to display a list of products in a category.
+#                      The function is also used in the search view to display a list of products that match the query.
+#                       The function is also used in the home view to display a list of products in a category.
+#                        The function is also used in the product detail view to display a list of related products.
+#                         The function is also used in the product detail view to display a list of products
+#                          in the category.
+#     :param request: request
+#     :param category_slug: str
+#     :return: render
+#     """
+#
+#     page = request.GET.get('page', 1)
+#     on_sale = request.GET.get('on_sale', None)
+#     order_by = request.GET.get('order_by', None)
+#     query = request.GET.get('q', None)
+#
+#     if category_slug == 'all':
+#         goods = Product.objects.all()
+#     elif query:
+#         goods = q_search(query)
+#     else:
+#         goods = get_list_or_404(Product.objects.filter(catalog__slug=category_slug))
+#
+#     if on_sale:
+#         goods = goods.filter(discount__gt=0)
+#
+#     if order_by and order_by != 'default':
+#         goods = goods.order_by(order_by)
+#
+#     paginator = Paginator(goods, 9)
+#     current_page = paginator.page(page)
+#
+#     context = {
+#         'title': 'Home - Catalog',
+#         'goods': current_page,
+#         'slag_url': category_slug
+#     }
+#     return render(request, 'goods/catalog.html', context)
 
+from django.shortcuts import redirect
+
+def catalog(request, category_slug=None):
     """
     Catalog view function that displays a list of products in a category.
-        The function takes a request and a category slug as input.
-         The function gets the category with the given slug from the database.
-        If the category slug is 'all', the function gets all products from the database.
-         If the category slug is not 'all', the function gets all products in the category from the database.
-          The function gets the page number from the request. The function gets the on_sale parameter from the request.
-           The function gets the order_by parameter from the request.
-            The function gets the query parameter from the request.
-             If the on_sale parameter is True, the function filters the products to only include products that
-              are on sale.
-              If the order_by parameter is not 'default', the function orders the products by the given parameter.
-               If the query parameter is not None, the function searches for products that match the query.
-                The function uses the q_search function to search for products that match the query.
-                 The function paginates the products and returns a list of products for the current page.
-                  The function returns a render of the catalog.html template with the list of products
-                   for the current page.
-                    The function is used in the catalog view to display a list of products in a category.
-                     The function is also used in the search view to display a list of products that match the query.
-                      The function is also used in the home view to display a list of products in a category.
-                       The function is also used in the product detail view to display a list of related products.
-                        The function is also used in the product detail view to display a list of products
-                         in the category.
     :param request: request
     :param category_slug: str
     :return: render
@@ -42,7 +80,10 @@ def catalog(request, category_slug=None):
     if category_slug == 'all':
         goods = Product.objects.all()
     elif query:
-        goods = q_search(query)
+        try:
+            goods = q_search(query)
+        except ValueError:
+            return redirect('catalog:index', category_slug=category_slug)
     else:
         goods = get_list_or_404(Product.objects.filter(catalog__slug=category_slug))
 
@@ -61,6 +102,7 @@ def catalog(request, category_slug=None):
         'slag_url': category_slug
     }
     return render(request, 'goods/catalog.html', context)
+
 
 
 def product(request, products_slug):

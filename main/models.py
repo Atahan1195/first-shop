25df00bc6267d4ae2b77b55225c0cbbd982f8f15
Atahan <atahan11951195@gmail.com>
2024-03-21 05:26:32 +0400

    address

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import EmailValidator
 from django.db import models
 
 
@@ -5,16 +6,16 @@ class About(models.Model):
 
     """
     This model holds information about the company or organization.
-    It includes the following fields: about, address, phone, email, and telegram.
+    It includes the following fields: about, address, phone, email, facebook, instagram, and telegram.
     """
 
-    about = models.TextField(blank=True, null=True)
-    phone = models.CharField(max_length=255)
-    address = models.URLField(blank=True, null=True)
-    facebook = models.URLField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    instagram = models.URLField(blank=True, null=True)
-    telegram = models.URLField(blank=True, null=True)
+    about = models.TextField(blank=True, null=True, max_length=255)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    address = models.URLField(blank=True, null=True, max_length=255)
+    facebook = models.URLField(blank=True, null=True, max_length=255)
+    email = models.EmailField(blank=True, null=True, max_length=255)
+    instagram = models.URLField(blank=True, null=True, max_length=255)
+    telegram = models.URLField(blank=True, null=True, max_length=255)
 
 
 
